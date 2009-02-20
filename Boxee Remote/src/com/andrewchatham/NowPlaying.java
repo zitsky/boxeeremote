798f9772f6420d3f067f8a420647bfc15a342f33
chatham <chatham@83f3234e-fee3-11dd-85c2-bb0660c75f22>
2009-02-20 00:28:40 +0000

    initial commit

---
+++ Boxee Remote/src/com/andrewchatham/NowPlaying.java
@@ -0,0 +1,29 @@
+package com.andrewchatham;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+final class NowPlaying {
+	final static Pattern LIST_ITEM = Pattern.compile("^<li>([A-Za-z]+):([^\n<]+)", Pattern.MULTILINE);
+	
+	private HashMap<String, String> mEntries;
+	
+	NowPlaying(String s) {
+		Matcher m = LIST_ITEM.matcher(s);
+		HashMap<String, String> entries = new HashMap<String, String>();
+		mEntries = entries;
+		while (m.find()) {
+			entries.put(m.group(1), m.group(2));
+		}
+	}
+	
+	String getFilename() {
+		return mEntries.get("Filename");
+	}
+	
+	String getThumbnailUrl() {
+		return mEntries.get("Thumb");
+	}
+	
+}
