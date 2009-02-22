b4456083cb7fb072c045ead5d99ecbe926d1e533
chatham <chatham@83f3234e-fee3-11dd-85c2-bb0660c75f22>
2009-02-22 03:41:45 +0000

    fix signature generation

--- trunk/Boxee Remote/src/com/andrewchatham/Discoverer.java
+++ trunk/Boxee Remote/src/com/andrewchatham/Discoverer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -20,11 +21,17 @@ import android.util.Log;
 public class Discoverer extends Thread {
 	private static final String TAG = "Discovery";
 	private static final String REMOTE_KEY = "b0xeeRem0tE!";
-	
+	private static final int DISCOVERY_PORT = 2562;
+	private static final int TIMEOUT_MS = 5000;
+
 	// TODO: Vary the challenge, or it's not much of a challenge :)
-	private static final String mChallenge = "My voice is my passport. Verify me.";
+	private static final String mChallenge = "myvoice";
 	private WifiManager mWifi;
-	
+
+	interface DiscoveryReceiver {
+		void addAnnouncedServers(InetAddress[] host, int port[]);
+	}
+
 	Discoverer(WifiManager wifi) {
 		mWifi = wifi;
 	}
@@ -32,14 +39,19 @@ public class Discoverer extends Thread {
 	public void run() {
 		try {
 			sendDiscoveryRequest();
+			listenForResponses();
 		} catch (IOException e) {
 			Log.e(TAG, "Could not send discovery request", e);
 		}
 	}
-	
-	private void sendDiscoveryRequest() throws IOException {
-		final int DISCOVERY_PORT = 2562;
 
+	/**
+	 * Send a broadcast UDP packet containing a request for boxee services to
+	 * announce themselves.
+	 * 
+	 * @throws IOException
+	 */
+	private void sendDiscoveryRequest() throws IOException {
 		String data = String
 				.format(
 						"<bdp1 cmd=\"discover\" application=\"iphone_remote\" challenge=\"%s\" signature=\"%s\"/>",
@@ -53,14 +65,18 @@ public class Discoverer extends Thread {
 		} else {
 			Log.d(TAG, "Dhcp info: " + dhcp);
 		}
-		
+
+		// Calculate the broadcast IP we need to send the packet along. If we
+		// send it to 255.255.255.255, it never gets sent. I guess this has
+		// something to do with the mobile network not wanting to do broadcast.
+
 		int broadcast = (dhcp.ipAddress & dhcp.netmask) | ~dhcp.netmask;
 		byte[] address = new byte[4];
 		for (int k = 0; k < 4; k++)
 			address[k] = (byte) ((broadcast >> k * 8) & 0xFF);
 		InetAddress group = InetAddress.getByAddress(address);
 		Log.d(TAG, "Send broadcast to " + group);
-		
+
 		DatagramPacket packet = new DatagramPacket(data.getBytes(), data
 				.length(), group, DISCOVERY_PORT);
 		DatagramSocket socket = new DatagramSocket();
@@ -68,6 +84,31 @@ public class Discoverer extends Thread {
 		socket.send(packet);
 	}
 
+	private void listenForResponses() throws IOException {
+		DatagramSocket socket = new DatagramSocket(DISCOVERY_PORT);
+		socket.setSoTimeout(TIMEOUT_MS);
+		byte[] buf = new byte[2048];
+		try {
+			while (true) {
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+				if (packet.getLength() > 0) {
+					Log.d(TAG, "Received response " + buf);
+				} else {
+					Log.d(TAG, "Received empty response");
+				}
+			}
+		} catch (SocketTimeoutException e) {
+			Log.d(TAG, "Receive timed out");
+		}
+	}
+
+	/**
+	 * Calculate the signature we need to send with the request. It is a string
+	 * containing the hex md5sum of the challenge and REMOTE_KEY.
+	 * 
+	 * @return signature string
+	 */
 	private String getSignature() {
 		MessageDigest digest;
 		byte[] md5sum = null;
@@ -82,7 +123,7 @@ public class Discoverer extends Thread {
 
 		StringBuffer hexString = new StringBuffer();
 		for (int k = 0; k < md5sum.length; ++k) {
-			String s = Integer.toHexString(md5sum[k]);
+			String s = Integer.toHexString((int) md5sum[k] & 0xFF);
 			if (s.length() == 1)
 				hexString.append('0');
 			hexString.append(s);
