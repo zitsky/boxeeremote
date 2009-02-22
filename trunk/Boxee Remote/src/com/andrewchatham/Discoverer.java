33ac8364f09f9c5241940cfb7a0d96798127616a
chatham <chatham@83f3234e-fee3-11dd-85c2-bb0660c75f22>
2009-02-22 20:28:18 +0000

    autodiscovery hooked into ui

--- trunk/Boxee Remote/src/com/andrewchatham/Discoverer.java
+++ trunk/Boxee Remote/src/com/andrewchatham/Discoverer.java
@@ -1,6 +1,7 @@
 package com.andrewchatham;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.io.StringReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -9,6 +10,7 @@ import java.net.InetSocketAddress;
 import java.net.SocketTimeoutException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -23,11 +25,18 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import android.net.DhcpInfo;
 import android.net.wifi.WifiManager;
+import android.os.Parcel;
 import android.util.Log;
 
-class BoxeeServer {
-  HashMap<String, String> mAttr;
+/**
+ * Code for dealing with Boxee server discovery. We send a broadcast UDP packet and wait for responses.
+ */
 
+/**
+ * Holds information about a Boxee server which announced itself in response to
+ * a discovery request.
+ */
+class BoxeeServer {
   private String mVersion;
   private String mName;
   private boolean mAuthRequired;
@@ -36,13 +45,20 @@ class BoxeeServer {
 
   public BoxeeServer(HashMap<String, String> attributes, InetAddress address) {
     mAddr = address;
-    mAttr = attributes;
     mVersion = attributes.get("version");
     mName = attributes.get("name");
-    mPort = Integer.parseInt(attributes.get("httpPort"));
+    try {
+      mPort = Integer.parseInt(attributes.get("httpPort"));
+    } catch (NumberFormatException e) {
+      mPort = BoxeeRemote.BAD_PORT;
+    }
 
     String auth = attributes.get("httpAuthRequired");
     mAuthRequired = auth != null && auth.equals("true");
+  }
+
+  public boolean valid() {
+    return mPort != BoxeeRemote.BAD_PORT && mAddr != null;
   }
 
   public String version() {
@@ -61,10 +77,12 @@ class BoxeeServer {
     return mPort;
   }
 
+  public InetAddress address() {
+    return mAddr;
+  }
+
   public String toString() {
-    return String.format("addr=%s port=%d version=%s name=%s auth_required=%s",
-        mAddr, mPort,
-        mVersion, mName, mAuthRequired);
+    return String.format("%s at %s:%d %s", mName, mAddr.getHostAddress(), mPort, valid() ? "" : "(broken?)");
   }
 }
 
@@ -78,30 +96,41 @@ public class Discoverer extends Thread {
   private static final String REMOTE_KEY = "b0xeeRem0tE!";
   private static final int DISCOVERY_PORT = 2562;
   private static final int TIMEOUT_MS = 500;
+  private Receiver mReceiver;
 
   // TODO: Vary the challenge, or it's not much of a challenge :)
   private static final String mChallenge = "myvoice";
   private WifiManager mWifi;
 
-  interface DiscoveryReceiver {
-    void addAnnouncedServers(InetAddress[] host, int port[]);
+  interface Receiver {
+    /**
+     * Process the list of discovered servers. This is always called once after
+     * a short timeout.
+     * 
+     * @param servers
+     *          list of discovered servers, null on error
+     */
+    void addAnnouncedServers(ArrayList<BoxeeServer> servers);
   }
 
-  Discoverer(WifiManager wifi) {
+  Discoverer(WifiManager wifi, Receiver receiver) {
     mWifi = wifi;
+    mReceiver = receiver;
   }
 
   public void run() {
+    ArrayList<BoxeeServer> servers = null;
     try {
       DatagramSocket socket = new DatagramSocket(DISCOVERY_PORT);
       socket.setBroadcast(true);
       socket.setSoTimeout(TIMEOUT_MS);
 
       sendDiscoveryRequest(socket);
-      listenForResponses(socket);
+      servers = listenForResponses(socket);
     } catch (IOException e) {
       Log.e(TAG, "Could not send discovery request", e);
     }
+    mReceiver.addAnnouncedServers(servers);
   }
 
   /**
@@ -146,24 +175,38 @@ public class Discoverer extends Thread {
    * 
    * @param socket
    *          socket on which the announcement request was sent
+   * @return list of discovered servers, never null
    * @throws IOException
    */
-  private void listenForResponses(DatagramSocket socket) throws IOException {
+  private ArrayList<BoxeeServer> listenForResponses(DatagramSocket socket)
+      throws IOException {
+    long start = System.currentTimeMillis();
     byte[] buf = new byte[1024];
+    ArrayList<BoxeeServer> servers = new ArrayList<BoxeeServer>();
+
+    // Loop and try to receive responses until the timeout elapses. We'll get
+    // back the packet we just sent out, which isn't terribly helpful, but we'll
+    // discard it in parseResponse because the cmd is wrong.
     try {
       while (true) {
         DatagramPacket packet = new DatagramPacket(buf, buf.length);
         socket.receive(packet);
         String s = new String(packet.getData(), 0, packet.getLength());
-        Log.d(TAG, "Received response " + s);
-        parseResponse(s, ((InetSocketAddress) packet.getSocketAddress()).getAddress());
+        Log.d(TAG, "Packet received after "
+            + (System.currentTimeMillis() - start) + " " + s);
+        BoxeeServer server = parseResponse(s, ((InetSocketAddress) packet
+            .getSocketAddress()).getAddress());
+        if (server != null)
+          servers.add(server);
       }
     } catch (SocketTimeoutException e) {
       Log.d(TAG, "Receive timed out");
     }
+    return servers;
   }
 
-  private void parseResponse(String response, InetAddress address) throws IOException {
+  private BoxeeServer parseResponse(String response, InetAddress address)
+      throws IOException {
     SAXParserFactory spf = SAXParserFactory.newInstance();
     SAXParser sp;
     final HashMap<String, String> values = new HashMap<String, String>();
@@ -201,25 +244,26 @@ public class Discoverer extends Thread {
       if (!legit_response.equals(signature.toLowerCase())) {
         Log.e(TAG, "Signature verification failed " + legit_response + " vs "
             + signature);
-        return;
+        return null;
       }
     }
-    Log.d(TAG, "Legit signature");
 
     String cmd = values.get("cmd");
     String app = values.get("application");
-    
+
     if (cmd == null || !cmd.equals("found")) {
+      // We'll get the discovery packet we sent out, where cmd="discovery"
       Log.e(TAG, "Bad cmd " + response);
-      return;
+      return null;
     }
-    if (app ==null || !app.equals("boxee")) {
+    if (app == null || !app.equals("boxee")) {
       Log.e(TAG, "Bad app " + app);
-      return;
+      return null;
     }
 
     BoxeeServer server = new BoxeeServer(values, address);
     Log.d(TAG, "Discovered server " + server);
+    return server;
   }
 
   /**
@@ -251,7 +295,7 @@ public class Discoverer extends Thread {
   }
 
   public static void main(String[] args) {
-    new Discoverer(null).start();
+    new Discoverer(null, null).start();
     while (true) {
     }
   }
