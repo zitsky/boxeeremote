53657f17774931c60082e84a223b8fbe9c8c35a6
chatham <chatham@83f3234e-fee3-11dd-85c2-bb0660c75f22>
2009-02-22 06:34:13 +0000

    receiving works

--- Boxee Remote/src/com/andrewchatham/Discoverer.java
+++ Boxee Remote/src/com/andrewchatham/Discoverer.java
@@ -5,6 +5,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -22,7 +23,7 @@ public class Discoverer extends Thread {
 	private static final String TAG = "Discovery";
 	private static final String REMOTE_KEY = "b0xeeRem0tE!";
 	private static final int DISCOVERY_PORT = 2562;
-	private static final int TIMEOUT_MS = 5000;
+	private static final int TIMEOUT_MS = 500;
 
 	// TODO: Vary the challenge, or it's not much of a challenge :)
 	private static final String mChallenge = "myvoice";
@@ -38,8 +39,12 @@ public class Discoverer extends Thread {
 
 	public void run() {
 		try {
-			sendDiscoveryRequest();
-			listenForResponses();
+			DatagramSocket socket = new DatagramSocket(DISCOVERY_PORT);
+			socket.setBroadcast(true);
+			socket.setSoTimeout(TIMEOUT_MS);
+			
+			sendDiscoveryRequest(socket);
+			listenForResponses(socket);
 		} catch (IOException e) {
 			Log.e(TAG, "Could not send discovery request", e);
 		}
@@ -51,52 +56,45 @@ public class Discoverer extends Thread {
 	 * 
 	 * @throws IOException
 	 */
-	private void sendDiscoveryRequest() throws IOException {
+	private void sendDiscoveryRequest(DatagramSocket socket) throws IOException {
 		String data = String
 				.format(
 						"<bdp1 cmd=\"discover\" application=\"iphone_remote\" challenge=\"%s\" signature=\"%s\"/>",
-						mChallenge, getSignature());
-		Log.d(TAG, data);
+						mChallenge, getSignature(mChallenge));
+		Log.d(TAG, "Sending data " + data);
 
+		DatagramPacket packet = new DatagramPacket(data.getBytes(), data
+				.length(), getBroadcastAddress(), DISCOVERY_PORT);
+		socket.send(packet);
+	}
+	
+	/**
+	 * Calculate the broadcast IP we need to send the packet along. If we
+	 * send it to 255.255.255.255, it never gets sent. I guess this has
+	 * something to do with the mobile network not wanting to do broadcast.
+	 */
+	private InetAddress getBroadcastAddress() throws IOException {
 		DhcpInfo dhcp = mWifi.getDhcpInfo();
 		if (dhcp == null) {
 			Log.d(TAG, "Could not get dhcp info");
-			return;
-		} else {
-			Log.d(TAG, "Dhcp info: " + dhcp);
+			return null; 
 		}
-
-		// Calculate the broadcast IP we need to send the packet along. If we
-		// send it to 255.255.255.255, it never gets sent. I guess this has
-		// something to do with the mobile network not wanting to do broadcast.
-
+		
 		int broadcast = (dhcp.ipAddress & dhcp.netmask) | ~dhcp.netmask;
-		byte[] address = new byte[4];
-		for (int k = 0; k < 4; k++)
-			address[k] = (byte) ((broadcast >> k * 8) & 0xFF);
-		InetAddress group = InetAddress.getByAddress(address);
-		Log.d(TAG, "Send broadcast to " + group);
-
-		DatagramPacket packet = new DatagramPacket(data.getBytes(), data
-				.length(), group, DISCOVERY_PORT);
-		DatagramSocket socket = new DatagramSocket();
-		socket.setBroadcast(true);
-		socket.send(packet);
+		byte[] quads = new byte[4];
+ 		for (int k = 0; k < 4; k++)
+			quads[k] = (byte) ((broadcast >> k * 8) & 0xFF);
+ 		return InetAddress.getByAddress(quads);
 	}
-
-	private void listenForResponses() throws IOException {
-		DatagramSocket socket = new DatagramSocket(DISCOVERY_PORT);
-		socket.setSoTimeout(TIMEOUT_MS);
-		byte[] buf = new byte[2048];
+	
+	private void listenForResponses(DatagramSocket socket) throws IOException {
+		byte[] buf = new byte[1024];
 		try {
 			while (true) {
 				DatagramPacket packet = new DatagramPacket(buf, buf.length);
 				socket.receive(packet);
-				if (packet.getLength() > 0) {
-					Log.d(TAG, "Received response " + buf);
-				} else {
-					Log.d(TAG, "Received empty response");
-				}
+				String s = new String(packet.getData(), 0, packet.getLength());
+				Log.d(TAG, "Received response " + s);
 			}
 		} catch (SocketTimeoutException e) {
 			Log.d(TAG, "Receive timed out");
@@ -109,12 +107,12 @@ public class Discoverer extends Thread {
 	 * 
 	 * @return signature string
 	 */
-	private String getSignature() {
+	private String getSignature(String challenge) {
 		MessageDigest digest;
 		byte[] md5sum = null;
 		try {
 			digest = java.security.MessageDigest.getInstance("MD5");
-			digest.update(mChallenge.getBytes());
+			digest.update(challenge.getBytes());
 			digest.update(REMOTE_KEY.getBytes());
 			md5sum = digest.digest();
 		} catch (NoSuchAlgorithmException e) {
