55f1d3f782c1460fb3cb928fa9482471302e9c01
chatham <chatham@83f3234e-fee3-11dd-85c2-bb0660c75f22>
2009-02-24 04:19:48 +0000

    Title and duration

--- trunk/Boxee Remote/src/com/andrewchatham/Scan.java
+++ trunk/Boxee Remote/src/com/andrewchatham/Scan.java
@@ -1,9 +1,6 @@
 package com.andrewchatham;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import android.app.ListActivity;
 import android.content.Context;
