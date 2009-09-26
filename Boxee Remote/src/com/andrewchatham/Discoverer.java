1434d873c503a5d0721a1bae793cc654ae1c6eaf
cemeterysunshine <cemeterysunshine@83f3234e-fee3-11dd-85c2-bb0660c75f22>
2009-09-26 01:35:07 +0000

    Moved classes BlockingHttprequest, BoxeeServer, and interface Handler to their own files.

--- Boxee Remote/src/com/andrewchatham/Discoverer.java
+++ Boxee Remote/src/com/andrewchatham/Discoverer.java
@@ -27,64 +27,7 @@ import android.net.wifi.WifiManager;
 import android.util.Log;
 
 /**
- * Code for dealing with Boxee server discovery. We send a broadcast UDP packet and wait for responses.
- */
-
-/**
- * Holds information about a Boxee server which announced itself in response to
- * a discovery request.
- */
-class BoxeeServer {
-  private String mVersion;
-  private String mName;
-  private boolean mAuthRequired;
-  private int mPort;
-  private InetAddress mAddr;
-
-  public BoxeeServer(HashMap<String, String> attributes, InetAddress address) {
-    mAddr = address;
-    mVersion = attributes.get("version");
-    mName = attributes.get("name");
-    try {
-      mPort = Integer.parseInt(attributes.get("httpPort"));
-    } catch (NumberFormatException e) {
-      mPort = Remote.BAD_PORT;
-    }
-
-    String auth = attributes.get("httpAuthRequired");
-    mAuthRequired = auth != null && auth.equals("true");
-  }
-
-  public boolean valid() {
-    return mPort != Remote.BAD_PORT && mAddr != null;
-  }
-
-  public String version() {
-    return mVersion;
-  }
-
-  public String name() {
-    return mName;
-  }
-
-  public boolean authRequired() {
-    return mAuthRequired;
-  }
-
-  public int port() {
-    return mPort;
-  }
-
-  public InetAddress address() {
-    return mAddr;
-  }
-
-  public String toString() {
-    return String.format("%s at %s:%d %s", mName, mAddr.getHostAddress(), mPort, valid() ? "" : "(broken?)");
-  }
-}
-
-/*
+ * Code for dealing with Boxee server discovery. 
  * This class tries to send a broadcast UDP packet over your wifi network to
  * discover the boxee service.
  */
